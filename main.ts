a1dab4349ad9472d7e6baf17e15370a3b1748b10
Ali Rafieefar <aliraf62@gmail.com>
2023-09-14 17:52:54 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,27 @@
+let teamBScore = 0
+radio.setGroup(1)
+
 radio.onReceivedValue(function (name, value) {
     if (name == "TeamBScore" && value == 1) {
         teamBScore += 1
         if (teamBScore == 3) {
+            // Celebrate victory
             for (let index = 0; index < 3; index++) {
-                music.playMelody("C D E F G A B C5 ", 120)
+                music.playMelody("C5 B A G F E D C", 120)
                 basic.showIcon(IconNames.Happy)
                 basic.pause(500)
                 basic.clearScreen()
                 basic.pause(500)
             }
-            // Reset for next game
+            // Reset for the next game
             teamBScore = 0
         } else {
             basic.showNumber(teamBScore)
         }
     }
 })
-let teamBScore = 0
-radio.setGroup(1)
+
+// Regularly show the current score
 basic.forever(function () {
     basic.showNumber(teamBScore)
     basic.pause(2000)
